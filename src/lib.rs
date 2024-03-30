11dbcce878d73f4fbd24afa472a5db92cf68bedc
Lucas <lluc23@hotmail.com>
2024-03-31 00:12:40 +0100

    rm iterator, update libsqil versions
    Signed-off-by: Lucas <lluc23@hotmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,9 @@ mod databases;
 mod groups;
 mod locations;
 mod organizations;
-mod utils;
 
 pub use client::{
     DatabasesPlatform, GroupsPlatform, LocationsPlatform, OrganizationsPlatform,
     TursoClient,
 };
 pub use databases::RetrievedDatabase;
-
-#[cfg(feature = "utils")]
-pub use utils::RowsIter;
